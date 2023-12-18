61a14ea9d0715e5524bab9a898f480f73b301104
Rey Gonzalez <88794686+ReyLira@users.noreply.github.com>
2023-12-18 16:05:12 -0500

    Revert "[UPDATE] Securization"

--- src/main/java/com/soa/canete/attorney/web/AttorneyController.java
+++ src/main/java/com/soa/canete/attorney/web/AttorneyController.java
@@ -11,6 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/attorneyData")
 @RequiredArgsConstructor
 public class AttorneyController {
