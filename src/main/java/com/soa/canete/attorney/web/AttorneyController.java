9bf34e5a0e172a2cdb87206878a7aad27e94221d
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-12-18 16:29:33 -0500

    [UPDATE] Attorney crossOrigin

--- src/main/java/com/soa/canete/attorney/web/AttorneyController.java
+++ src/main/java/com/soa/canete/attorney/web/AttorneyController.java
@@ -11,7 +11,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/attorneyData")
 @RequiredArgsConstructor
 public class AttorneyController {
