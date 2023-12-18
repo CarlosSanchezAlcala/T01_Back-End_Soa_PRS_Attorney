3958f4a7418f90a5fb09cd9d92cb1fe37a992905
Carlos Edidson SANCHEZ ALCALA <89946636+CarlosSanchezAlcala@users.noreply.github.com>
2023-12-18 16:12:13 -0500

    Delete src directory

--- src/main/java/com/soa/canete/attorney/repository/AttorneyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soa.canete.attorney.repository;
-
-import com.soa.canete.attorney.domain.model.Attorney;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface AttorneyRepository extends ReactiveCrudRepository<Attorney, Integer> {
-}
